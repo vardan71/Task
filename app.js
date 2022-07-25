c50b44147da14d6783e78e99ac0706e0c114cac5
vardan71 <vardanhakobyan7771@gmail.com>
2022-07-25 14:31:03 +0400

    task6

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-function capitalize(value){
-      return value[0].toUpperCase() + value.substring(1);
-  }
-  const value = 'hello world';
-  console.log(capitalize(value))
+function isLowerCase(a) {
+      return a[0] !== a[0].toUpperCase();
+}
+function word(value) {
+      let words = value.split(' ')
+
+      for (let i = 0; i < words.length; i++) {
+            if (isLowerCase(words[i][0])) {
+                 words[i]= words[i].charAt(0).toUpperCase() + words[i].substring(1)
+            }
+      }
+      return words.join('')
+}
+
+
+console.log(word("JavaScript Exercises"));
+console.log(word("JavaScript exercises"));
+console.log(word("JavaScriptExercises"));
