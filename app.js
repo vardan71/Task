28b4800faae01c4e2e04656259a2950dd2172007
vardan71 <vardanhakobyan7771@gmail.com>
2022-07-27 18:17:26 +0400

    task11

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let colors = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
-let ordNum = ["th", "st", "nd", "rd"];
-
-for (let i = 0; i < colors.length; i++) {
-    if (i < 3) {
-        console.log(i + 1 + ordNum[i + 1] + ' choice is ' + colors[i])
-    } else {
-        console.log(i + 1 + ordNum[0] + ' choice is ' + colors[i])
+let num = [14, 2, 16, 8, 38, 33, 12, 4, 64, 7];
+let c;
+for (let j = 0; j < num.length - 1; j++) {
+    for (let i = 0; i < num.length - 1; i++) {
+        if (num[i] > num[i + 1]) {
+            c = num[i + 1];
+            num[i + 1] = num[i];
+            num[i] = c;
+        }
     }
-};
+}
+console.log(num);
